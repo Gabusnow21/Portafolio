dad0cf51e44e01b3e8f581154bbbc11bb9b04c58
¨Gabriel <dg22023@ues.edu.sv>
2026-08-25 17:21:54 -0600

    feat: add backend structure (Spring Boot) and frontend submodule

---
+++ portafolioback/src/test/java/com/dev/gabus/portafolioback/PortafoliobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.gabus.portafolioback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafoliobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
